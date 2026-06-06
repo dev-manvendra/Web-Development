367e9237eb61d3a8bf3d14661fdad0f0aebb51bd
Manvendra <manvendrajhansi1406@gmail.com>
2026-06-06 05:24:35 +0000

    conditional statement

---
+++ JavaScript/04_Control_Flow/01_intro.js
@@ -0,0 +1,16 @@
+// ther are total three control statement
+// 1. if- Statement
+// 2. else- Statement
+// 3. elseif- statement
+
+// if- Statement execute block of code if the given condition is true.
+if (!true) 
+{
+    console.log("The condition is ture")
+
+}
+// else statement execute block of code if the given condition is true.
+
+else{
+    console.log("The condition is not true")
+}
