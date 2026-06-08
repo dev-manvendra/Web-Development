6774185eb2576ec35aac6e4849732702eb31e6d0
Manvendra <manvendrajhansi1406@gmail.com>
2026-06-08 03:36:36 +0000

    filter method

--- JavaScript/05_Loops/04_filter_map_and_reduce.js
+++ JavaScript/05_Loops/04_filter_map_and_reduce.js
@@ -1,5 +1,19 @@
 const coding = ["Python", "JavaScript","Java", "c++"]
 
-coding.forEach(element => {
-    console.log("elements is :", element);
-});
+// coding.forEach(element => {
+//     console.log("elements is :", element);
+// });
+// foreach method does not return actual value
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Ther are some methods that take call back function and return value.
+
+///////////////////////// 1. filter() ////////////////////////////////////////////////////
+
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+let newnum = num.filter((n)=> n>4 ) // this method filter the data according to giving condition.
+
+console.log(newnum); 
+
