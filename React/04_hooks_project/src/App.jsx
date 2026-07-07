c67202f7619da9577da107fc0d31d88661a49ffc
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-07 12:01:39 +0530

    html and css complete

--- React/04_hooks_project/src/App.jsx
+++ React/04_hooks_project/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [color, setColor] = useState("olive")
 
   return (
-    <></>
-  )
+    <div className='main'
+    style={{backgroundColor: color}}>
+        <div className='heading'>
+          <h1>Background Colour Changer</h1>
+          <p>Click on the button below to change the background-color</p>
+        </div>
+        <div className='colour'>
+          <button className='btn'>Red</button>
+          <button className='btn'>Green</button>
+          <button className='btn'>Blue</button>
+          <button className='btn'>White</button>
+          <button className='btn'>Burgandy</button>
+          <button className='btn'>Yellow</button>
+          
+        </div>
+    </div>
+  );
 }
 
 export default App
