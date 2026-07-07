1175adb20dbbe081d55e2a191911a14e377c1eeb
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-07 08:00:25 +0530

    new updates

--- JavaScript/09_Advance_one/02_promises.js
+++ JavaScript/09_Advance_one/02_promises.js
@@ -1,14 +1,33 @@
-const promiseOne = new Promise((resolve, reject)=>{
+// const promiseOne = new Promise((resolve, reject)=>{
 
-    //
-    setTimeout(() => {
-        console.log("Asynchronous task is completed")
-        resolve()
-    },3000);
+//     //
+//     setTimeout(() => {
+//         console.log("Asynchronous task is completed")
+//         resolve()
+//     },3000);
 
-})
-
-promiseOne.then(()=>
-{
-    console.log("promises consumed");
+// })
+// promiseOne.then(()=>
+//     {
+//         console.log("promises consumed");
+//     }
+// )
+    
+let msg = "this is first message";
+const new_promise = new Promise((resolve, reject)=>{
+    let i = 0;
+    let setTimer = setInterval(()=>{
+        console.log(msg[i]);
+        i++;
+        if(i == msg.length)
+        {
+            clearInterval(setTimer);
+            setTimer = null;
+        }
+        
+    },500)
+}
+)
+new_promise.then(()=>{
+    console.log("Task complete");
 })
