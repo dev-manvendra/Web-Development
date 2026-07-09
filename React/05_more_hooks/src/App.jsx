cb488b8b77558688e565630fe8dea415a7930b96
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-09 10:57:12 +0530

    html (page format) complete

--- React/05_more_hooks/src/App.jsx
+++ React/05_more_hooks/src/App.jsx
@@ -7,6 +7,44 @@ function App() {
   return (
     <>
       <div className='main'>
+        <div>
+          <div className='input-field'>
+            <h1>Password Generator</h1>
+            <input 
+            type="text"
+            value="http"
+            placeholder='Password'
+            readOnly
+            />
+            <button>Copy</button>
+          </div>
+          <div className='fields'>
+            <div>
+                <input 
+                  type="range"
+                  min={8}
+                  max={16}
+                  value="10"
+                  className='scale'
+                  
+                />
+                <label >Length</label>
+            </div>
+            <div>
+                <input 
+                  type="checkbox"                
+                />
+                <label >Numbers</label>
+            </div>
+            <div>
+                <input 
+                  type="checkbox"                
+                />
+                <label >Characters</label>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </>
   )
