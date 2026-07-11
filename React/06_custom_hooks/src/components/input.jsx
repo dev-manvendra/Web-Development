c046fce0709b394aadb78b7b279132ac5820dff8
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-11 13:05:54 +0530

    set the attributes and parameters in inputbox components

--- React/06_custom_hooks/src/components/input.jsx
+++ React/06_custom_hooks/src/components/input.jsx
@@ -1,22 +1,31 @@
 
 function InputBox({
     label,
-    
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption =[],
+    selectCurrency = "usd",
+    amountDisabled = false,
+    currencyDisabled = false,
     className = "",
 }) {
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block">
-                    label
+                    {label}
                 </label>
                 <input
                     
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    disabled= {amountDisabled}
+                    value={amount}
+                    onChange={(e)=> onAmountChange && onAmountChange(Number(e.targer.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
