b425b84af2fb2650c041728d73d641b19086a885
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-11 08:58:11 +0530

    optimize the custom hooks for better performance

--- React/06_custom_hooks/src/hooks/useCurrencyInfo.js
+++ React/06_custom_hooks/src/hooks/useCurrencyInfo.js
@@ -1,19 +1,27 @@
 import { useState, useEffect } from "react";
 
 
-function useCurrencyInfo(currnecy) {
+function useCurrencyInfo(currency) {
     
     const [data, setData] = useState({});
 
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((response)=>{
+            if(!response.ok)
+            {
+                throw new Error("file not found");
+            }
             return response.json();
         })
         .then((response)=>{
-            setData =  response[currency];
+            setData(response[currency]);
+        })
+        .catch((error)=>{
+            console.error(error);
         });
-    }, [currnecy]);
+        
+    }, [currency]);
     
     return data;
 }
