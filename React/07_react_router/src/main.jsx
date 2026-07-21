cebb875bb2cbe385b3d927b921d57d51b02837c0
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-21 19:17:08 +0530

    add react router in our project

--- React/07_react_router/src/main.jsx
+++ React/07_react_router/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const router  = 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
