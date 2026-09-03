5f475b0c369aadbf6835bdbb53da2e017ca8f002
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-03 21:21:01 +0530

    Refactor code structure and remove redundant changes

--- Backend/Data-Modling/Todos/todos.model.js
+++ Backend/Data-Modling/Todos/todos.model.js
@@ -1,5 +1,23 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({})
+const UserSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,  
+        lowercase : true 
+    },
+    password : {
+        type : String, 
+        required : [true, "password is required"],
+        unique : true
+    }
+})
 
 export const User = mongoose.model("User", UserSchema);
