ef1fda63f323e57e80c73262fb2f5c34e992926f
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-11 15:10:06 +0530

    feat: initialize Express app and set up server listening in index.js

--- Backend/Youtube-Backend/src/index.js
+++ Backend/Youtube-Backend/src/index.js
@@ -1,12 +1,26 @@
 import dns from "node:dns"
 import "dotenv/config"
 import connectDB from "./db/index.js"
+import app from "./app.js"
+import { log } from "node:console"
 
 if (process.env.DNS_SERVERS) {
     dns.setServers(process.env.DNS_SERVERS.split(",").map(server => server.trim()))
 }
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("Server running at port", process.env.PORT);
+        
+    });
+
+    
+})
+.catch( (error)=>{
+    console.log("Database Connection Error!!", error);
+    
+})
 
 
 
