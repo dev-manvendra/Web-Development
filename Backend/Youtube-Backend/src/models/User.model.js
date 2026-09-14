8d1910769a23aaa242257b234f0a90342aabec68
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-14 22:40:13 +0530

    feat: add password hashing and comparison methods to User model

--- Backend/Youtube-Backend/src/models/User.model.js
+++ Backend/Youtube-Backend/src/models/User.model.js
@@ -51,6 +51,19 @@ const UserSchema = mongoose.Schema({
 
 },{timestamps : true});
 
+UserSchema.pre("save", async function (next) {
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+    
+})
+
+UserSchema.methods(async function (password) {
+    return await bcrypt.compare(password, this.password);
+    
+})
+
 export const User = mongoose.model("User", UserSchema);
 
 
