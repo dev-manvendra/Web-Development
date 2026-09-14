c93ba50c54bdacb93f9b11fef2e8b7c4074e339b
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-14 23:56:52 +0530

    feat: add methods for password verification and JWT token generation in User model

--- Backend/Youtube-Backend/src/models/User.model.js
+++ Backend/Youtube-Backend/src/models/User.model.js
@@ -59,9 +59,38 @@ UserSchema.pre("save", async function (next) {
     
 })
 
-UserSchema.methods(async function (password) {
+UserSchema.methods(verifyPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
     
+})
+
+UserSchema.methods(getAccessToken = function () {
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+        },
+       process.env.ACCESS_TOKEN_SECRET,
+       {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+       }
+
+    )
+})
+UserSchema.methods(getRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+        },
+       process.env.REFRESH_TOKEN_SECRET,
+       {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+       }
+
+    )
 })
 
 export const User = mongoose.model("User", UserSchema);
