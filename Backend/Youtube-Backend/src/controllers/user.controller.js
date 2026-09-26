0bf29c3a97276c33280470efe38286f51c864c64
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-09-26 23:42:48 +0530

    feat: add generateAccessAndRefreshTokens function for token management

--- Backend/Youtube-Backend/src/controllers/user.controller.js
+++ Backend/Youtube-Backend/src/controllers/user.controller.js
@@ -4,6 +4,28 @@ import {User} from "../models/User.model.js"
 import uploadOnCloudinary from "../utils/Cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+
+
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
+
+
+
 const userRegister = AsyncHandler(async (req, res) =>{
 
     // get user details from frontend
